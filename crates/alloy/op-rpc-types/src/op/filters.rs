dd3db7ed8126a06e4dfadb1f8e2e77c3831a76fd
EmperorOrokuSaki <artie.eth@gmail.com>
2024-04-12 13:18:53 +0400

    feat: add filters.rs

---
+++ crates/alloy/op-rpc-types/src/op/filters.rs
@@ -0,0 +1,69 @@
+use super::transaction::Transaction;
+use alloy::rpc::types::eth::Log as RpcLog;
+use alloy_primitives::B256;
+use serde::{Deserialize, Deserializer, Serialize};
+
+/// Response of the `eth_getFilterChanges` RPC.
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[serde(untagged)]
+pub enum FilterChanges {
+    /// Empty result.
+    #[serde(with = "empty_array")]
+    Empty,
+    /// New logs.
+    Logs(Vec<RpcLog>),
+    /// New hashes (block or transactions).
+    Hashes(Vec<B256>),
+    /// New transactions.
+    Transactions(Vec<Transaction>),
+}
+mod empty_array {
+    use serde::{Serialize, Serializer};
+
+    pub(super) fn serialize<S>(s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        (&[] as &[()]).serialize(s)
+    }
+}
+impl<'de> Deserialize<'de> for FilterChanges {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum Changes {
+            Hashes(Vec<B256>),
+            Logs(Vec<RpcLog>),
+            Transactions(Vec<Transaction>),
+        }
+
+        let changes = Changes::deserialize(deserializer)?;
+        let changes = match changes {
+            Changes::Logs(vals) => {
+                if vals.is_empty() {
+                    FilterChanges::Empty
+                } else {
+                    FilterChanges::Logs(vals)
+                }
+            }
+            Changes::Hashes(vals) => {
+                if vals.is_empty() {
+                    FilterChanges::Empty
+                } else {
+                    FilterChanges::Hashes(vals)
+                }
+            }
+            Changes::Transactions(vals) => {
+                if vals.is_empty() {
+                    FilterChanges::Empty
+                } else {
+                    FilterChanges::Transactions(vals)
+                }
+            }
+        };
+        Ok(changes)
+    }
+}
--- crates/alloy/op-rpc-types/src/op/mod.rs
+++ crates/alloy/op-rpc-types/src/op/mod.rs
@@ -1,5 +1,6 @@
 mod block;
 mod call;
+mod filters;
 mod pubsub;
 mod transaction;
 pub use alloy::rpc::types::eth::{Header, Log};
