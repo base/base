3c86458386c5a6db4248f980d08341237ae19d17
Matthias Seitz <matthias.seitz@outlook.de>
2024-08-26 11:21:44 +0200

    feat: add other op endpoints (#46)

--- crates/alloy/rpc-jsonrpsee/src/traits.rs
+++ crates/alloy/rpc-jsonrpsee/src/traits.rs
@@ -1,8 +1,17 @@
+#![allow(missing_docs)]
+
 //! Rollup Node
 
 use alloy_eips::BlockNumberOrTag;
+use alloy_primitives::B256;
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
-use op_alloy_rpc_types::{config::RollupConfig, output::OutputResponse, sync::SyncStatus};
+use op_alloy_rpc_types::{
+    config::RollupConfig,
+    net::{PeerDump, PeerInfo, PeerStats},
+    output::OutputResponse,
+    sync::SyncStatus,
+};
+use std::net::IpAddr;
 
 /// Optimism specified rpc interface.
 ///
@@ -27,4 +36,79 @@ pub trait RollupNode {
     /// Get the software version.
     #[method(name = "version")]
     async fn op_version(&self) -> RpcResult<String>;
+}
+
+/// The opp2p namespace handles peer interactions.
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "opp2p"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "opp2p"))]
+pub trait OpP2PApi {
+    /// Returns information of node
+    #[method(name = "self")]
+    async fn opp2p_self(&self) -> RpcResult<PeerInfo>;
+
+    #[method(name = "peers")]
+    async fn opp2p_peers(&self) -> RpcResult<PeerDump>;
+
+    #[method(name = "peerStats")]
+    async fn opp2p_peer_stats(&self) -> RpcResult<PeerStats>;
+
+    #[method(name = "discoveryTable")]
+    async fn opp2p_discovery_table(&self) -> RpcResult<Vec<String>>;
+
+    #[method(name = "blockPeer")]
+    async fn opp2p_block_peer(&self, peer: String) -> RpcResult<()>;
+
+    #[method(name = "listBlockedPeers")]
+    async fn opp2p_list_blocked_peers(&self) -> RpcResult<Vec<String>>;
+
+    #[method(name = "blocAddr")]
+    async fn opp2p_block_addr(&self, ip: IpAddr) -> RpcResult<()>;
+
+    #[method(name = "unblockAddr")]
+    async fn opp2p_unblock_addr(&self, ip: IpAddr) -> RpcResult<()>;
+
+    #[method(name = "listBlockedAddrs")]
+    async fn opp2p_list_blocked_addrs(&self) -> RpcResult<Vec<IpAddr>>;
+
+    /// todo: should be IPNet?
+    #[method(name = "blockSubnet")]
+    async fn opp2p_block_subnet(&self, subnet: String) -> RpcResult<()>;
+
+    /// todo: should be IPNet?
+    #[method(name = "unblockSubnet")]
+    async fn opp2p_unblock_subnet(&self, subnet: String) -> RpcResult<()>;
+
+    /// todo: should be IPNet?
+    #[method(name = "listBlockedSubnets")]
+    async fn opp2p_list_blocked_subnets(&self) -> RpcResult<Vec<String>>;
+
+    #[method(name = "protectPeer")]
+    async fn opp2p_protect_peer(&self, peer: String) -> RpcResult<()>;
+
+    #[method(name = "unprotectPeer")]
+    async fn opp2p_unprotect_peer(&self, peer: String) -> RpcResult<()>;
+
+    #[method(name = "connectPeer")]
+    async fn opp2p_connect_peer(&self, peer: String) -> RpcResult<()>;
+
+    #[method(name = "disconnectPeer")]
+    async fn opp2p_disconnect_peer(&self, peer: String) -> RpcResult<()>;
+}
+
+/// The admin namespace endpoints
+/// https://github.com/ethereum-optimism/optimism/blob/c7ad0ebae5dca3bf8aa6f219367a95c15a15ae41/op-node/node/api.go#L28-L36
+#[cfg_attr(not(feature = "client"), rpc(server, namespace = "admin"))]
+#[cfg_attr(feature = "client", rpc(server, client, namespace = "admin"))]
+pub trait OpAdminApi {
+    #[method(name = "resetDerivationPipeline")]
+    async fn admin_reset_derivation_pipeline(&self) -> RpcResult<()>;
+
+    #[method(name = "startSequencer")]
+    async fn admin_start_sequencer(&self, block_hash: B256) -> RpcResult<()>;
+
+    #[method(name = "stopSequencer")]
+    async fn admin_stop_sequencer(&self) -> RpcResult<B256>;
+
+    #[method(name = "sequencerActive")]
+    async fn admin_sequencer_active(&self) -> RpcResult<bool>;
 }
