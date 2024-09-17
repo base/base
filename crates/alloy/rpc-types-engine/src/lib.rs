5c074db496ac602e67e3976e1989aba9918e4ca2
refcell <abigger87@gmail.com>
2024-09-17 05:42:45 -0400

    fix: no_std for op-alloy-rpc-types-engine (#109)

--- crates/alloy/rpc-types-engine/src/lib.rs
+++ crates/alloy/rpc-types-engine/src/lib.rs
@@ -14,6 +14,7 @@
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
 
