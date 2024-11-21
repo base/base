7859dd61e393e67fcaffbf8808f0cb8311dd6fac
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-11-20 16:30:36 -0800

    feat: Add default block range (#217)

--- crates/op-succinct/proposer/op/op_proposer.sh
+++ crates/op-succinct/proposer/op/op_proposer.sh
@@ -15,7 +15,7 @@
     --max-concurrent-proof-requests=${MAX_CONCURRENT_PROOF_REQUESTS:-10} \
     --db-path=${DB_PATH:-/usr/local/bin/dbdata} \
     --op-succinct-server-url=${OP_SUCCINCT_SERVER_URL:-http://op-succinct-server:3000} \
-    --max-block-range-per-span-proof=${MAX_BLOCK_RANGE_PER_SPAN_PROOF:-20} \
+    --max-block-range-per-span-proof=${MAX_BLOCK_RANGE_PER_SPAN_PROOF:-300} \
     --use-cached-db=${USE_CACHED_DB:-false} \
     --metrics.enabled=${METRICS_ENABLED:-true} \
     --metrics.port=${METRICS_PORT:-7300} \
