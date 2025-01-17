2bc3c05f220cfb2690fd16afd7f19966707828bb
Emiliano Bonassi <emiliano@conduit.xyz>
2025-01-18 00:43:05 +0100

    feat(contracts): various deployment improvements (#321)
    * feat(contracts): support reusing existing deployed implementation (savings $80)
    
    * feat(contracts): support proxy managed via ProxyAdmin (default op configuration)
    
    * feat(contracts): optionally use different key for deployment
    
    * chore: improve
    
    * add
    
    * wip
    
    * add
    
    * add
    
    * fix
    
    * add
    
    ---------
    
    Co-authored-by: Ratan Kaliani <ratankaliani@berkeley.edu>

--- crates/op-succinct/contracts/script/OPSuccinctUpgrader.s.sol
+++ crates/op-succinct/contracts/script/OPSuccinctUpgrader.s.sol
@@ -9,16 +9,40 @@ import {console} from "forge-std/console.sol";
 
 contract OPSuccinctUpgrader is Script, Utils {
     function run() public {
-        vm.startBroadcast();
-
         Config memory cfg = readJson("opsuccinctl2ooconfig.json");
 
         address l2OutputOracleProxy = vm.envAddress("L2OO_ADDRESS");
         bool executeUpgradeCall = vm.envOr("EXECUTE_UPGRADE_CALL", true);
 
-        address OPSuccinctL2OutputOracleImpl = address(new OPSuccinctL2OutputOracle());
+        // Use implementation address from config
+        address OPSuccinctL2OutputOracleImpl = cfg.opSuccinctL2OutputOracleImpl;
+        address proxyAdmin = cfg.proxyAdmin;
 
-        upgradeAndInitialize(OPSuccinctL2OutputOracleImpl, cfg, l2OutputOracleProxy, executeUpgradeCall);
+        // optionally use a different key for deployment
+        uint256 deployPk = vm.envOr("DEPLOY_PK", uint256(0));
+        uint256 adminPk = vm.envOr("ADMIN_PK", uint256(0));
+
+        // If deployPk is not set, use the default key.
+        if (deployPk != uint256(0)) {
+            vm.startBroadcast(deployPk);
+        } else {
+            vm.startBroadcast();
+        }
+
+        if (OPSuccinctL2OutputOracleImpl == address(0)) {
+            console.log("Deploying new OPSuccinctL2OutputOracle impl");
+            OPSuccinctL2OutputOracleImpl = address(new OPSuccinctL2OutputOracle());
+        }
+
+        vm.stopBroadcast();
+
+        if (adminPk != uint256(0)) {
+            vm.startBroadcast(adminPk);
+        } else {
+            vm.startBroadcast();
+        }
+
+        upgradeAndInitialize(cfg, l2OutputOracleProxy, executeUpgradeCall);
 
         vm.stopBroadcast();
     }
