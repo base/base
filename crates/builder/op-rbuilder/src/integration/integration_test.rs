874a9dff16296669c5657bee39fa47a84b736dfe
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-02-11 10:41:42 +0000

    Spawn a task with the Op builder txn monitoring instead of ExEx (#417)
    ## 📝 Summary
    
    Closes #412. Using the same scaffolding of the monitoring task I added
    support to track the node updates from a canonical stream from the
    provider (which is the same primitive used by the exex). Then, I use a
    spawned task to run the new monitoring with the stream once the node has
    been initialized.
    
    This is a patch until ExEx is fixed or we outsource the monitoring to an
    external entity.
    
    ## 💡 Motivation and Context
    
    <!--- (Optional) Why is this change required? What problem does it
    solve? Remove this section if not applicable. -->
    
    ---
    
    ## ✅ I have completed the following steps:
    
    * [x] Run `make lint`
    * [x] Run `make test`
    * [x] Added tests (if applicable)

--- crates/builder/op-rbuilder/src/integration/integration_test.rs
+++ crates/builder/op-rbuilder/src/integration/integration_test.rs
@@ -7,6 +7,9 @@ mod tests {
     use std::path::PathBuf;
     use uuid::Uuid;
 
+    const BUILDER_PRIVATE_KEY: &str =
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+
     #[tokio::test]
     async fn integration_test_chain_produces_blocks() {
         // This is a simple test using the integration framework to test that the chain
@@ -20,13 +23,12 @@ mod tests {
 
         // create the builder
         let builder_data_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
-        let builder = OpRbuilderConfig::new()
+        let op_rbuilder_config = OpRbuilderConfig::new()
             .chain_config_path(genesis_path.clone())
             .data_dir(builder_data_dir)
             .auth_rpc_port(1234)
-            .network_port(1235);
-
-        framework.start("op-rbuilder", &builder).await.unwrap();
+            .network_port(1235)
+            .with_builder_private_key(BUILDER_PRIVATE_KEY);
 
         // create the validation reth node
         let reth_data_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
@@ -38,6 +40,11 @@ mod tests {
 
         framework.start("op-reth", &reth).await.unwrap();
 
+        let op_rbuilder = framework
+            .start("op-rbuilder", &op_rbuilder_config)
+            .await
+            .unwrap();
+
         let engine_api = EngineApi::new("http://localhost:1234").unwrap();
         let validation_api = EngineApi::new("http://localhost:1236").unwrap();
 
@@ -47,5 +54,11 @@ mod tests {
         for _ in 0..10 {
             generator.generate_block().await.unwrap();
         }
+
+        // there must be a line logging the monitoring transaction
+        op_rbuilder
+            .find_log_line("Committed block built by builder")
+            .await
+            .unwrap();
     }
 }
