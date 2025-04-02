5753b631b65f3437089ce58d654f76f64318fcd3
Ratan Kaliani <ratan@succinct.xyz>
2025-04-02 16:22:13 -0700

    chore(programs): bump `kona` (#470)
    Co-authored-by: Ubuntu <ubuntu@large-op-succinct-dev.maas>

--- crates/op-succinct/utils/host/src/stats.rs
+++ crates/op-succinct/utils/host/src/stats.rs
@@ -137,7 +137,7 @@ impl ExecutionStats {
             batch_start: block_data[0].block_number - 1,
             batch_end: block_data[block_data.len() - 1].block_number,
             total_instruction_count: report.total_instruction_count(),
-            total_sp1_gas: 0,
+            total_sp1_gas: report.gas.unwrap_or(0),
             block_execution_instruction_count: get_cycles("block-execution"),
             oracle_verify_instruction_count: get_cycles("oracle-verify"),
             derivation_instruction_count: get_cycles("payload-derivation"),
