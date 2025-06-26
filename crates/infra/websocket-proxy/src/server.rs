flashbots/rollup-boost

feat: add ws filtering by addresses and topics with configurable match logic

crates/websocket-proxy/src/server.rs
@@ -1,20 +1,22 @@
 use crate::auth::Authentication;
 use crate::client::ClientConnection;
+use crate::filter::{FilterType, MatchMode};
 use crate::metrics::Metrics;
 use crate::rate_limit::{RateLimit, RateLimitError};
 use crate::registry::Registry;
 use axum::body::Body;
-use axum::extract::{ConnectInfo, Path, State, WebSocketUpgrade};
+use axum::extract::{ConnectInfo, Path, Query, State, WebSocketUpgrade};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::{any, get};
 use axum::{Error, Router};
 use http::{HeaderMap, HeaderValue};
+use serde::Deserialize;
 use serde_json::json;
 use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
 use tokio_util::sync::CancellationToken;
-use tracing::{info, warn};
+use tracing::{info, debug, warn};
 
 #[derive(Clone)]
 struct ServerState {
@@ -33,6 +35,13 @@ pub struct Server {
     metrics: Arc<Metrics>,
     ip_addr_http_header: String,
     authentication: Option<Authentication>,
+}
+
+#[derive(Deserialize)]
+struct FilterQuery {
+    addresses: Option<String>,
+    topics: Option<String>,
+    r#match: Option<String>,
 }
 
 impl Server {
@@ -59,10 +68,14 @@ impl Server {
 
         if self.authentication.is_some() {
             info!("Authentication is enabled");
-            router = router.route("/ws/{api_key}", any(authenticated_websocket_handler));
+            router = router
+                .route("/ws/{api_key}", any(authenticated_websocket_handler))
+                .route("/ws/{api_key}/filter", any(authenticated_filter_websocket_handler));
         } else {
             info!("Public endpoint is enabled");
-            router = router.route("/ws", any(unauthenticated_websocket_handler));
+            router = router
+                .route("/ws", any(unauthenticated_websocket_handler))
+                .route("/ws/filter", any(unauthenticated_filter_websocket_handler));
         }
 
         let router = router.with_state(ServerState {
@@ -99,6 +112,38 @@ async fn healthz_handler() -> impl IntoResponse {
     StatusCode::OK
 }
 
+// Parse comma-separated values into Vec<String>
+fn parse_comma_separated(input: Option<String>) -> Vec<String> {
+    input
+        .map(|s| {
+            s.split(',')
+                .map(|item| item.trim().to_string())
+                .filter(|item| !item.is_empty())
+                .collect()
+        })
+        .unwrap_or_default()
+}
+
+fn create_filter_from_query(query: FilterQuery) -> FilterType {
+    let addresses = parse_comma_separated(query.addresses);
+    let topics = parse_comma_separated(query.topics);
+
+    // Parse match mode, default to "any" if not specified
+    let match_mode = match query.r#match.as_deref() {
+        Some("all") => MatchMode::All,
+        Some("any") | None => MatchMode::Any,
+        Some(other) => {
+            warn!("Invalid match parameter '{}', defaulting to 'any'", other);
+            MatchMode::Any
+        }
+    };
+
+    let filter = FilterType::new_combined_with_mode(addresses.clone(), topics.clone(), match_mode);
+    debug!("Created filter: {:?} from addresses: {:?}, topics: {:?}, match_mode: {:?}",
+           filter, addresses, topics, match_mode);
+    filter
+}
+
 async fn authenticated_websocket_handler(
     State(state): State<ServerState>,
     ws: WebSocketUpgrade,
@@ -121,7 +166,36 @@ async fn authenticated_websocket_handler(
         }
         Some(app) => {
             state.metrics.proxy_connections_by_app(app);
-            websocket_handler(state, ws, addr, headers)
+            websocket_handler(state, ws, addr, headers, FilterType::None)
+        }
+    }
+}
+
+async fn authenticated_filter_websocket_handler(
+    State(state): State<ServerState>,
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    headers: HeaderMap,
+    Path(api_key): Path<String>,
+    query: Query<FilterQuery>,
+) -> impl IntoResponse {
+    let application = state.auth.get_application_for_key(&api_key);
+
+    match application {
+        None => {
+            state.metrics.unauthorized_requests.increment(1);
+
+            Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .body(Body::from(
+                    json!({"message": "Invalid API key"}).to_string(),
+                ))
+                .unwrap()
+        }
+        Some(app) => {
+            state.metrics.proxy_connections_by_app(app);
+            let filter = create_filter_from_query(query.0);
+            websocket_handler(state, ws, addr, headers, filter)
         }
     }
 }
@@ -132,7 +206,18 @@ async fn unauthenticated_websocket_handler(
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     headers: HeaderMap,
 ) -> impl IntoResponse {
-    websocket_handler(state, ws, addr, headers)
+    websocket_handler(state, ws, addr, headers, FilterType::None)
+}
+
+async fn unauthenticated_filter_websocket_handler(
+    State(state): State<ServerState>,
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    headers: HeaderMap,
+    query: Query<FilterQuery>,
+) -> impl IntoResponse {
+    let filter = create_filter_from_query(query.0);
+    websocket_handler(state, ws, addr, headers, filter)
 }
 
 fn websocket_handler(
@@ -140,6 +225,7 @@ fn websocket_handler(
     ws: WebSocketUpgrade,
     addr: SocketAddr,
     headers: HeaderMap,
+    filter: FilterType,
 ) -> Response {
     let connect_addr = addr.ip();
 
@@ -168,7 +254,7 @@ fn websocket_handler(
         )
     })
     .on_upgrade(async move |socket| {
-        let client = ClientConnection::new(client_addr, ticket, socket);
+        let client = ClientConnection::new(client_addr, ticket, socket, filter);
         state.registry.subscribe(client).await;
     })
 }
@@ -204,7 +290,84 @@ fn extract_addr(header: &HeaderValue, fallback: IpAddr) -> IpAddr {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::filter::FilterType;
     use std::net::Ipv4Addr;
+
+    #[test]
+    fn test_parse_comma_separated() {
+        // Test single value
+        let single = Some("0x123".to_string());
+        assert_eq!(parse_comma_separated(single), vec!["0x123"]);
+
+        // Test multiple values
+        let multiple = Some("0x123, 0x456 ,0x789".to_string());
+        assert_eq!(
+            parse_comma_separated(multiple),
+            vec!["0x123", "0x456", "0x789"]
+        );
+
+        // Test empty
+        assert_eq!(parse_comma_separated(None), Vec::<String>::new());
+
+        // Test empty string
+        let empty = Some("".to_string());
+        assert_eq!(parse_comma_separated(empty), Vec::<String>::new());
+
+        // Test with empty values
+        let with_empty = Some("0x123,,0x456".to_string());
+        assert_eq!(parse_comma_separated(with_empty), vec!["0x123", "0x456"]);
+    }
+
+    #[test]
+    fn test_create_filter_from_query() {
+        // Test addresses only
+        let query = FilterQuery {
+            addresses: Some("0x123,0x456".to_string()),
+            topics: None,
+            r#match: None,
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::Addresses(_) => (),
+            _ => panic!("Expected Addresses filter"),
+        }
+
+        // Test topics only
+        let query = FilterQuery {
+            addresses: None,
+            topics: Some("0xabc,0xdef".to_string()),
+            r#match: None,
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::Topics(_) => (),
+            _ => panic!("Expected Topics filter"),
+        }
+
+        // Test combined
+        let query = FilterQuery {
+            addresses: Some("0x123".to_string()),
+            topics: Some("0xabc".to_string()),
+            r#match: Some("all".to_string()),
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::Combined { .. } => (),
+            _ => panic!("Expected Combined filter"),
+        }
+
+        // Test none
+        let query = FilterQuery {
+            addresses: None,
+            topics: None,
+            r#match: None,
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::None => (),
+            _ => panic!("Expected None filter"),
+        }
+    }
 
     #[tokio::test]
     async fn test_header_addr() {

base/base

crates/infra/websocket-proxy/src/server.rs
@@ -1,20 +1,22 @@
 use crate::auth::Authentication;
 use crate::client::ClientConnection;
+use crate::filter::{FilterType, MatchMode};
 use crate::metrics::Metrics;
 use crate::rate_limit::{RateLimit, RateLimitError};
 use crate::registry::Registry;
 use axum::body::Body;
-use axum::extract::{ConnectInfo, Path, State, WebSocketUpgrade};
+use axum::extract::{ConnectInfo, Path, Query, State, WebSocketUpgrade};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::{any, get};
 use axum::{Error, Router};
 use http::{HeaderMap, HeaderValue};
+use serde::Deserialize;
 use serde_json::json;
 use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
 use tokio_util::sync::CancellationToken;
-use tracing::{info, warn};
+use tracing::{debug, info, warn};
 
 #[derive(Clone)]
 struct ServerState {
@@ -33,6 +35,14 @@ pub struct Server {
     metrics: Arc<Metrics>,
     ip_addr_http_header: String,
     authentication: Option<Authentication>,
+    public_access_enabled: bool,
+}
+
+#[derive(Deserialize)]
+struct FilterQuery {
+    addresses: Option<String>,
+    topics: Option<String>,
+    r#match: Option<String>,
 }
 
 impl Server {
@@ -43,6 +53,7 @@ impl Server {
         rate_limiter: Arc<dyn RateLimit>,
         authentication: Option<Authentication>,
         ip_addr_http_header: String,
+        public_access_enabled: bool,
     ) -> Self {
         Self {
             listen_addr,
@@ -51,6 +62,7 @@ impl Server {
             metrics,
             authentication,
             ip_addr_http_header,
+            public_access_enabled,
         }
     }
 
@@ -59,8 +71,18 @@ impl Server {
 
         if self.authentication.is_some() {
             info!("Authentication is enabled");
-            router = router.route("/ws/{api_key}", any(authenticated_websocket_handler));
+            router = router
+                .route("/ws/{api_key}", any(authenticated_websocket_handler))
+                .route(
+                    "/ws/{api_key}/filter",
+                    any(authenticated_filter_websocket_handler),
+                );
         } else {
+            info!("Public endpoint is enabled");
+            router = router.route("/ws", any(unauthenticated_websocket_handler));
+        }
+
+        if self.public_access_enabled && self.authentication.is_some() {
             info!("Public endpoint is enabled");
             router = router.route("/ws", any(unauthenticated_websocket_handler));
         }
@@ -99,6 +121,40 @@ async fn healthz_handler() -> impl IntoResponse {
     StatusCode::OK
 }
 
+// Parse comma-separated values into Vec<String>
+fn parse_comma_separated(input: Option<String>) -> Vec<String> {
+    input
+        .map(|s| {
+            s.split(',')
+                .map(|item| item.trim().to_string())
+                .filter(|item| !item.is_empty())
+                .collect()
+        })
+        .unwrap_or_default()
+}
+
+fn create_filter_from_query(query: FilterQuery) -> FilterType {
+    let addresses = parse_comma_separated(query.addresses);
+    let topics = parse_comma_separated(query.topics);
+
+    // Parse match mode, default to "any" if not specified
+    let match_mode = match query.r#match.as_deref() {
+        Some("all") => MatchMode::All,
+        Some("any") | None => MatchMode::Any,
+        Some(other) => {
+            warn!("Invalid match parameter '{}', defaulting to 'any'", other);
+            MatchMode::Any
+        }
+    };
+
+    let filter = FilterType::new_combined_with_mode(addresses.clone(), topics.clone(), match_mode);
+    debug!(
+        "Created filter: {:?} from addresses: {:?}, topics: {:?}, match_mode: {:?}",
+        filter, addresses, topics, match_mode
+    );
+    filter
+}
+
 async fn authenticated_websocket_handler(
     State(state): State<ServerState>,
     ws: WebSocketUpgrade,
@@ -121,7 +177,36 @@ async fn authenticated_websocket_handler(
         }
         Some(app) => {
             state.metrics.proxy_connections_by_app(app);
-            websocket_handler(state, ws, addr, headers)
+            websocket_handler(state, ws, addr, headers, FilterType::None)
+        }
+    }
+}
+
+async fn authenticated_filter_websocket_handler(
+    State(state): State<ServerState>,
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    headers: HeaderMap,
+    Path(api_key): Path<String>,
+    query: Query<FilterQuery>,
+) -> impl IntoResponse {
+    let application = state.auth.get_application_for_key(&api_key);
+
+    match application {
+        None => {
+            state.metrics.unauthorized_requests.increment(1);
+
+            Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .body(Body::from(
+                    json!({"message": "Invalid API key"}).to_string(),
+                ))
+                .unwrap()
+        }
+        Some(app) => {
+            state.metrics.proxy_connections_by_app(app);
+            let filter = create_filter_from_query(query.0);
+            websocket_handler(state, ws, addr, headers, filter)
         }
     }
 }
@@ -132,7 +217,7 @@ async fn unauthenticated_websocket_handler(
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     headers: HeaderMap,
 ) -> impl IntoResponse {
-    websocket_handler(state, ws, addr, headers)
+    websocket_handler(state, ws, addr, headers, FilterType::None)
 }
 
 fn websocket_handler(
@@ -140,6 +225,7 @@ fn websocket_handler(
     ws: WebSocketUpgrade,
     addr: SocketAddr,
     headers: HeaderMap,
+    filter: FilterType,
 ) -> Response {
     let connect_addr = addr.ip();
 
@@ -168,7 +254,7 @@ fn websocket_handler(
         )
     })
     .on_upgrade(async move |socket| {
-        let client = ClientConnection::new(client_addr, ticket, socket);
+        let client = ClientConnection::new(client_addr, ticket, socket, filter);
         state.registry.subscribe(client).await;
     })
 }
@@ -204,7 +290,84 @@ fn extract_addr(header: &HeaderValue, fallback: IpAddr) -> IpAddr {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::filter::FilterType;
     use std::net::Ipv4Addr;
+
+    #[test]
+    fn test_parse_comma_separated() {
+        // Test single value
+        let single = Some("0x123".to_string());
+        assert_eq!(parse_comma_separated(single), vec!["0x123"]);
+
+        // Test multiple values
+        let multiple = Some("0x123, 0x456 ,0x789".to_string());
+        assert_eq!(
+            parse_comma_separated(multiple),
+            vec!["0x123", "0x456", "0x789"]
+        );
+
+        // Test empty
+        assert_eq!(parse_comma_separated(None), Vec::<String>::new());
+
+        // Test empty string
+        let empty = Some("".to_string());
+        assert_eq!(parse_comma_separated(empty), Vec::<String>::new());
+
+        // Test with empty values
+        let with_empty = Some("0x123,,0x456".to_string());
+        assert_eq!(parse_comma_separated(with_empty), vec!["0x123", "0x456"]);
+    }
+
+    #[test]
+    fn test_create_filter_from_query() {
+        // Test addresses only
+        let query = FilterQuery {
+            addresses: Some("0x123,0x456".to_string()),
+            topics: None,
+            r#match: None,
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::Addresses(_) => (),
+            _ => panic!("Expected Addresses filter"),
+        }
+
+        // Test topics only
+        let query = FilterQuery {
+            addresses: None,
+            topics: Some("0xabc,0xdef".to_string()),
+            r#match: None,
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::Topics(_) => (),
+            _ => panic!("Expected Topics filter"),
+        }
+
+        // Test combined
+        let query = FilterQuery {
+            addresses: Some("0x123".to_string()),
+            topics: Some("0xabc".to_string()),
+            r#match: Some("all".to_string()),
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::Combined { .. } => (),
+            _ => panic!("Expected Combined filter"),
+        }
+
+        // Test none
+        let query = FilterQuery {
+            addresses: None,
+            topics: None,
+            r#match: None,
+        };
+        let filter = create_filter_from_query(query);
+        match filter {
+            FilterType::None => (),
+            _ => panic!("Expected None filter"),
+        }
+    }
 
     #[tokio::test]
     async fn test_header_addr() {