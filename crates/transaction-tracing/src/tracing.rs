a4c6b8a0dc2941091e7416cce647cae31d05ea69
William Law <williamlaw.wtl@gmail.com>
2025-09-09 15:42:30 -0400

    [metrics] track tx lifecycle time (#75)
    * wip exex
    
    * first pass
    
    * remove readme
    
    * track num dropped/replaced
    
    * update toml structure
    
    * naming
    
    * nits
    
    * use lru over hashmap
    
    * record metrics
    
    * always install exex
    
    * fix ci
    
    * combine inclusion and event fn together
    
    * use install if
    
    * fix ci

---
+++ crates/transaction-tracing/src/tracing.rs
@@ -0,0 +1,143 @@
+use alloy_primitives::TxHash;
+use eyre::Result;
+use futures::StreamExt;
+use lru::LruCache;
+use reth::api::{BlockBody, FullNodeComponents};
+use reth::core::primitives::{AlloyBlockHeader, SignedTransaction};
+use reth::transaction_pool::{FullTransactionEvent, TransactionPool};
+use reth_exex::{ExExContext, ExExEvent, ExExNotification};
+use reth_tracing::tracing::debug;
+use std::num::NonZeroUsize;
+use std::time::Instant;
+use tokio_stream::wrappers::ReceiverStream;
+
+/// Types of transaction events to track
+#[derive(Debug)]
+enum TxEvent {
+    Dropped,
+    Replaced,
+    BlockInclusion,
+}
+
+/// Simple ExEx that tracks transaction timing from mempool to inclusion
+struct Tracker {
+    /// Map of transaction hash to timestamp when first seen in mempool
+    txs: LruCache<TxHash, Instant>,
+}
+
+impl Tracker {
+    fn new() -> Self {
+        Self {
+            txs: LruCache::new(NonZeroUsize::new(20000).unwrap()),
+        }
+    }
+
+    fn transaction_inserted(&mut self, tx_hash: TxHash) {
+        let now = Instant::now();
+        self.txs.put(tx_hash, now);
+        debug!(target: "transaction-tracing", tx_hash = ?tx_hash, "Transaction added to mempool");
+    }
+
+    // Handle transaction event (drop, replace, block inclusion, etc.)
+    fn transaction_event(&mut self, tx_hash: TxHash, event: TxEvent) {
+        if let Some(mempool_time) = self.txs.pop(&tx_hash) {
+            let time_in_mempool = Instant::now().duration_since(mempool_time);
+
+            // Record histogram with event label
+            let event_label = match event {
+                TxEvent::Dropped => "dropped",
+                TxEvent::Replaced => "replaced",
+                TxEvent::BlockInclusion => "block_inclusion",
+            };
+            metrics::histogram!("reth_transaction_tracing_tx_event", "event" => event_label)
+                .record(time_in_mempool.as_millis() as f64);
+
+            debug!(
+                target: "transaction-tracing",
+                tx_hash = ?tx_hash,
+                event = ?event,
+                time_in_mempool = ?time_in_mempool.as_millis(),
+                "Transaction event",
+            );
+        } else {
+            debug!(
+                target: "transaction-tracing",
+                tx_hash = ?tx_hash,
+                "Transaction included in block (not tracked by ExEx)",
+            );
+        }
+    }
+}
+
+pub async fn transaction_tracing_exex<Node: FullNodeComponents>(
+    mut ctx: ExExContext<Node>,
+) -> Result<()> {
+    debug!(target: "transaction-tracing", "Starting transaction tracking ExEx");
+
+    let mut track = Tracker::new();
+
+    // Subscribe to events from the mempool
+    let pool = ctx.pool().clone();
+    let mempool_receiver = pool.pending_transactions_listener();
+    let mut mempool_stream = ReceiverStream::new(mempool_receiver);
+
+    // Subscribe to all transaction events to detect drops/replacements
+    let mut all_events_stream = pool.all_transactions_event_listener();
+
+    loop {
+        tokio::select! {
+            // New events from the mempool
+            Some(tx_hash) = mempool_stream.next() => {
+                track.transaction_inserted(tx_hash);
+            }
+
+            // Track # of transactions dropped and replaced
+            Some(full_event) = all_events_stream.next() => {
+                match full_event {
+                    FullTransactionEvent::Discarded(tx_hash) => {
+                        track.transaction_event(tx_hash, TxEvent::Dropped);
+                    }
+                    FullTransactionEvent::Replaced{transaction, replaced_by: _} => {
+                        let tx_hash = transaction.hash();
+                        track.transaction_event(*tx_hash, TxEvent::Replaced);
+                    }
+                    _ => {
+                        // Other events (mined, replaced, etc.)
+                    }
+                }
+            }
+
+            // Use chain notifications to track time to inclusion
+            Some(notification) = ctx.notifications.next() => {
+                match notification {
+                    Ok(ExExNotification::ChainCommitted { new }) => {
+                        // Process all transactions in committed chain
+                        for block in new.blocks().values() {
+                            for transaction in block.body().transactions() {
+                                track.transaction_event(*transaction.tx_hash(), TxEvent::BlockInclusion);
+                            }
+                        }
+                        ctx.events.send(ExExEvent::FinishedHeight(new.tip().num_hash()))?;
+                    }
+                    Ok(ExExNotification::ChainReorged { old: _, new }) => {
+                        debug!(target: "transaction-tracing", tip = ?new.tip().number(), "Chain reorg detected");
+                        for block in new.blocks().values() {
+                            for transaction in block.body().transactions() {
+                                track.transaction_event(*transaction.tx_hash(), TxEvent::BlockInclusion);
+                            }
+                        }
+                        ctx.events.send(ExExEvent::FinishedHeight(new.tip().num_hash()))?;
+                    }
+                    Ok(ExExNotification::ChainReverted { old }) => {
+                        debug!(target: "transaction-tracing", old_tip = ?old.tip().number(), "Chain reverted");
+                        ctx.events.send(ExExEvent::FinishedHeight(old.tip().num_hash()))?;
+                    }
+                    Err(e) => {
+                        debug!(target: "transaction-tracing", "Notification error: {}", e);
+                        return Err(e);
+                    }
+                }
+            }
+        }
+    }
+}
