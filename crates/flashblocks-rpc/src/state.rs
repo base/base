1f6a508c69f7a4f0b6601611bb1d9442db744b4f
Haardik H <haardik.haardik@coinbase.com>
2025-10-28 12:40:01 -0400

    log canon block number being used

--- crates/flashblocks-rpc/src/state.rs
+++ crates/flashblocks-rpc/src/state.rs
@@ -397,8 +397,10 @@ where
         let earliest_block_number = flashblocks_per_block.keys().min().unwrap();
         let canonical_block = earliest_block_number - 1;
         let mut last_block_header = self.client.header_by_number(canonical_block)?.ok_or(eyre!(
-            "Failed to extract header for canonical block number {}. This is okay if your node is not fully synced to tip yet.",
-            canonical_block
+            "Failed to extract header for canonical block number {}. This is okay if your node is not fully synced to tip yet. Earliest block number {}. Flashblocks per block: {:?}",
+            canonical_block,
+            earliest_block_number,
+            flashblocks_per_block.keys(),
         ))?;
 
         let evm_config = OpEvmConfig::optimism(self.client.chain_spec());
