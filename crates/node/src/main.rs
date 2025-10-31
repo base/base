569f1483f0a6177ef626ddce6a5b68ad356d572e
Julian Meyer <julian.meyer@coinbase.com>
2025-10-31 12:16:13 -0700

    WIP

--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -1,4 +1,7 @@
-use std::sync::Arc;
+use std::{
+    path::PathBuf,
+    sync::{Arc, LazyLock, OnceLock},
+};
 
 use base_reth_flashblocks_rpc::{
     rpc::{EthApiExt, EthApiOverrideServer},
@@ -8,16 +11,28 @@ use base_reth_flashblocks_rpc::{
 use base_reth_metering::{MeteringApiImpl, MeteringApiServer};
 use base_reth_transaction_tracing::transaction_tracing_exex;
 use clap::Parser;
-use futures_util::TryStreamExt;
+use futures_util::{FutureExt, TryStreamExt};
+use jsonrpsee::{
+    core::{async_trait, RpcResult},
+    proc_macros::rpc,
+};
 use once_cell::sync::OnceCell;
 use reth::{
+    api::FullNodeComponents,
     builder::{EngineNodeLauncher, Node, NodeHandle, TreeConfig},
     providers::providers::BlockchainProvider,
+    rpc::result::internal_rpc_err,
     version::{default_reth_version_metadata, try_init_version_metadata, RethCliVersionConsts},
 };
 use reth_exex::ExExEvent;
 use reth_optimism_cli::{chainspec::OpChainSpecParser, Cli};
+use reth_optimism_exex::OpProofsExEx;
 use reth_optimism_node::{args::RollupArgs, OpNode};
+use reth_optimism_rpc::{
+    debug::{DebugApiExt, DebugApiOverrideServer},
+    eth::proofs::{EthApiExt as OpEthApiExt, EthApiOverrideServer as OpEthApiOverrideServer},
+};
+use reth_optimism_trie::{db::MdbxProofsStorage, OpProofsStorage, OpProofsStore};
 use tracing::info;
 use url::Url;
 
@@ -49,6 +64,19 @@ struct Args {
     /// Enable metering RPC for transaction bundle simulation
     #[arg(long = "enable-metering", value_name = "ENABLE_METERING")]
     pub enable_metering: bool,
+
+    /// If true, initialize external-proofs exex to save and serve trie nodes to provide proofs
+    /// faster.
+    #[arg(long = "proofs-history", value_name = "PROOFS_HISTORY", default_value = "false")]
+    pub proofs_history: bool,
+
+    /// The path to the storage DB for proofs history.
+    #[arg(
+        long = "proofs-history.storage-path",
+        value_name = "PROOFS_HISTORY_STORAGE_PATH",
+        required_if_eq("proofs_history", "true")
+    )]
+    pub proofs_history_storage_path: Option<PathBuf>,
 }
 
 impl Args {
@@ -83,11 +111,44 @@ fn main() {
             info!(message = "starting custom Base node");
 
             let flashblocks_enabled = args.flashblocks_enabled();
+            let proofs_history_enabled = args.proofs_history;
+
             let transaction_tracing_enabled = args.enable_transaction_tracing;
             let metering_enabled = args.enable_metering;
             let op_node = OpNode::new(args.rollup_args.clone());
 
-            let fb_cell: Arc<OnceCell<Arc<FlashblocksState<_>>>> = Arc::new(OnceCell::new());
+            let fb_cell: Arc<OnceLock<Arc<FlashblocksState<_>>>> = Arc::new(OnceLock::new());
+
+            let proofs_storage_cell =
+                Arc::new(LazyLock::new(|| -> Arc<OpProofsStorage<MdbxProofsStorage>> {
+                    let path = args
+                        .proofs_history_storage_path
+                        .expect("path must be set when proofs history is enabled");
+                    let result: Arc<OpProofsStorage<MdbxProofsStorage>> = Arc::new(
+                        MdbxProofsStorage::new(&path).expect("Failed to create MdbxProofsStorage"),
+                    )
+                    .into();
+
+                    result
+                }));
+
+            let proofs_storage_cell_exex = proofs_storage_cell.clone();
+            let proofs_storage_cell_rpc = proofs_storage_cell.clone();
+            let proofs_storage_cell =
+                Arc::new(LazyLock::new(|| -> Arc<OpProofsStorage<MdbxProofsStorage>> {
+                    let path = args
+                        .proofs_history_storage_path
+                        .expect("path must be set when proofs history is enabled");
+                    let result: Arc<OpProofsStorage<MdbxProofsStorage>> = Arc::new(
+                        MdbxProofsStorage::new(&path).expect("Failed to create MdbxProofsStorage"),
+                    )
+                    .into();
+
+                    result
+                }));
+
+            let proofs_storage_cell_exex = proofs_storage_cell.clone();
+            let proofs_storage_cell_rpc = proofs_storage_cell.clone();
 
             let NodeHandle { node: _node, node_exit_future } = builder
                 .with_types_and_provider::<OpNode, BlockchainProvider<_>>()
@@ -122,6 +183,20 @@ fn main() {
                         })
                     }
                 })
+                .install_exex_if(
+                    proofs_history_enabled,
+                    "proofs-history",
+                    move |exex_context| async move {
+                        let proofs_storage = (*proofs_storage_cell_exex).clone();
+                        Ok(OpProofsExEx::new(
+                            exex_context,
+                            proofs_storage,
+                            0, // TODO: unused
+                        )
+                        .run()
+                        .boxed())
+                    },
+                )
                 .extend_rpc_modules(move |ctx| {
                     if metering_enabled {
                         info!(message = "Starting Metering RPC");
@@ -139,6 +214,7 @@ fn main() {
                         )?;
 
                         let fb = fb_cell
+                            .clone()
                             .get_or_init(|| Arc::new(FlashblocksState::new(ctx.provider().clone())))
                             .clone();
                         fb.start();
@@ -155,6 +231,25 @@ fn main() {
                         ctx.modules.replace_configured(api_ext.into_rpc())?;
                     } else {
                         info!(message = "flashblocks integration is disabled");
+                    }
+                    Ok(())
+                })
+                .extend_rpc_modules(move |ctx| {
+                    if proofs_history_enabled {
+                        let proofs_storage = (*(proofs_storage_cell_rpc)).clone();
+                        let api_ext = OpEthApiExt::new(
+                            ctx.registry.eth_api().clone(),
+                            proofs_storage.clone(),
+                        );
+                        let debug_ext = DebugApiExt::new(
+                            ctx.node().provider().clone(),
+                            ctx.registry.eth_api().clone(),
+                            proofs_storage.clone(),
+                            Box::new(ctx.node().task_executor().clone()),
+                            ctx.node().evm_config().clone(),
+                        );
+                        ctx.modules.replace_configured(api_ext.into_rpc())?;
+                        ctx.modules.replace_configured(debug_ext.into_rpc())?;
                     }
                     Ok(())
                 })
