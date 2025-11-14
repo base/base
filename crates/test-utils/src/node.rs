1b4ba2a9ca55c545e15fd0b74f210e8e7366ea0e
Niran Babalola <niran.babalola@coinbase.com>
2025-11-14 12:08:04 -0600

    test-utils: wire flashblocks state into harness

--- crates/test-utils/src/node.rs
+++ crates/test-utils/src/node.rs
@@ -4,8 +4,12 @@ use crate::engine::EngineApi;
 use alloy_genesis::Genesis;
 use alloy_provider::RootProvider;
 use alloy_rpc_client::RpcClient;
+use base_reth_flashblocks_rpc::rpc::{EthApiExt, EthApiOverrideServer};
+use base_reth_flashblocks_rpc::state::FlashblocksState;
+use base_reth_flashblocks_rpc::subscription::{Flashblock, FlashblocksReceiver};
 use eyre::Result;
 use futures_util::Future;
+use once_cell::sync::OnceCell;
 use op_alloy_network::Optimism;
 use reth::api::{FullNodeTypesAdapter, NodeTypesWithDBAdapter};
 use reth::args::{DiscoveryArgs, NetworkArgs, RpcServerArgs};
@@ -15,6 +19,7 @@ use reth::builder::{
 use reth::core::exit::NodeExitFuture;
 use reth::tasks::TaskManager;
 use reth_e2e_test_utils::{Adapter, TmpDB};
+use reth_exex::ExExEvent;
 use reth_optimism_chainspec::OpChainSpec;
 use reth_optimism_node::args::RollupArgs;
 use reth_optimism_node::OpNode;
@@ -22,13 +27,20 @@ use reth_provider::providers::BlockchainProvider;
 use std::any::Any;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use tokio::sync::{mpsc, oneshot};
+use tokio_stream::StreamExt;
 
 pub const BASE_CHAIN_ID: u64 = 84532;
+
+pub type LocalNodeProvider = BlockchainProvider<NodeTypesWithDBAdapter<OpNode, TmpDB>>;
+pub type LocalFlashblocksState = FlashblocksState<LocalNodeProvider>;
 
 pub struct LocalNode {
     pub(crate) http_api_addr: SocketAddr,
     engine_ipc_path: String,
-    // flashblock_sender: mpsc::Sender<(Flashblock, oneshot::Sender<()>)>,
+    flashblock_sender: mpsc::Sender<(Flashblock, oneshot::Sender<()>)>,
+    flashblocks_state: Arc<LocalFlashblocksState>,
+    provider: LocalNodeProvider,
     _node_exit_future: NodeExitFuture,
     _node: Box<dyn Any + Sync + Send>,
     _task_manager: TaskManager,
@@ -93,8 +105,9 @@ impl LocalNode {
 
         let node = OpNode::new(RollupArgs::default());
 
-        // let (sender, mut receiver) = mpsc::channel::<(Flashblock, oneshot::Sender<()>)>(100);
-        // let fb_cell: Arc<OnceCell<Arc<FlashblocksState<_>>>> = Arc::new(OnceCell::new());
+        let (sender, receiver) = mpsc::channel::<(Flashblock, oneshot::Sender<()>)>(100);
+        let fb_cell: Arc<OnceCell<Arc<FlashblocksState<_>>>> = Arc::new(OnceCell::new());
+        let provider_cell: Arc<OnceCell<LocalNodeProvider>> = Arc::new(OnceCell::new());
 
         let NodeHandle {
             node: node_handle,
@@ -104,47 +117,59 @@ impl LocalNode {
             .with_types_and_provider::<OpNode, BlockchainProvider<_>>()
             .with_components(node.components_builder())
             .with_add_ons(node.add_ons())
-            // .install_exex("flashblocks-canon", {
-            //     let fb_cell = fb_cell.clone();
-            //     move |mut ctx| async move {
-            //         let fb = fb_cell
-            //             .get_or_init(|| Arc::new(FlashblocksState::new(ctx.provider().clone())))
-            //             .clone();
-            //         Ok(async move {
-            //             while let Some(note) = ctx.notifications.try_next().await? {
-            //                 if let Some(committed) = note.committed_chain() {
-            //                     for b in committed.blocks_iter() {
-            //                         fb.on_canonical_block_received(b);
-            //                     }
-            //                     let _ = ctx
-            //                         .events
-            //                         .send(ExExEvent::FinishedHeight(committed.tip().num_hash()));
-            //                 }
-            //             }
-            //             Ok(())
-            //         })
-            //     }
-            // })
-            // .extend_rpc_modules(move |ctx| {
-            //     let fb = fb_cell
-            //         .get_or_init(|| Arc::new(FlashblocksState::new(ctx.provider().clone())))
-            //         .clone();
-            //     fb.start();
-            //     let api_ext = EthApiExt::new(
-            //         ctx.registry.eth_api().clone(),
-            //         ctx.registry.eth_handlers().filter.clone(),
-            //         fb.clone(),
-            //     );
-            //     ctx.modules.replace_configured(api_ext.into_rpc())?;
-            //     // Spawn task to receive flashblocks from the test context
-            //     tokio::spawn(async move {
-            //         while let Some((payload, tx)) = receiver.recv().await {
-            //             fb.on_flashblock_received(payload);
-            //             tx.send(()).unwrap();
-            //         }
-            //     });
-            //     Ok(())
-            // })
+            .install_exex("flashblocks-canon", {
+                let fb_cell = fb_cell.clone();
+                let provider_cell = provider_cell.clone();
+                move |mut ctx| async move {
+                    let provider = provider_cell.get_or_init(|| ctx.provider().clone()).clone();
+                    let fb = fb_cell
+                        .get_or_init(|| Arc::new(FlashblocksState::new(provider.clone())))
+                        .clone();
+                    Ok(async move {
+                        while let Some(note) = ctx.notifications.try_next().await? {
+                            if let Some(committed) = note.committed_chain() {
+                                for block in committed.blocks_iter() {
+                                    fb.on_canonical_block_received(block);
+                                }
+                                let _ = ctx
+                                    .events
+                                    .send(ExExEvent::FinishedHeight(committed.tip().num_hash()));
+                            }
+                        }
+                        Ok(())
+                    })
+                }
+            })
+            .extend_rpc_modules({
+                let fb_cell = fb_cell.clone();
+                let provider_cell = provider_cell.clone();
+                let mut receiver = Some(receiver);
+                move |ctx| {
+                    let provider = provider_cell.get_or_init(|| ctx.provider().clone()).clone();
+                    let fb = fb_cell
+                        .get_or_init(|| Arc::new(FlashblocksState::new(provider.clone())))
+                        .clone();
+                    fb.start();
+                    let api_ext = EthApiExt::new(
+                        ctx.registry.eth_api().clone(),
+                        ctx.registry.eth_handlers().filter.clone(),
+                        fb.clone(),
+                    );
+                    ctx.modules.replace_configured(api_ext.into_rpc())?;
+                    // Spawn task to receive flashblocks from the test context
+                    let fb_for_task = fb.clone();
+                    let mut receiver = receiver
+                        .take()
+                        .expect("flashblock receiver should only be initialized once");
+                    tokio::spawn(async move {
+                        while let Some((payload, tx)) = receiver.recv().await {
+                            fb_for_task.on_flashblock_received(payload);
+                            let _ = tx.send(());
+                        }
+                    });
+                    Ok(())
+                }
+            })
             .launch_with_fn(launcher)
             .await?;
 
@@ -154,23 +179,36 @@ impl LocalNode {
             .ok_or_else(|| eyre::eyre!("HTTP RPC server failed to bind to address"))?;
 
         let engine_ipc_path = node_config.rpc.auth_ipc_path;
+        let flashblocks_state = fb_cell
+            .get()
+            .expect("FlashblocksState should be initialized during node launch")
+            .clone();
+        let provider = provider_cell
+            .get()
+            .expect("Provider should be initialized during node launch")
+            .clone();
 
         Ok(Self {
             http_api_addr,
             engine_ipc_path,
-            // flashblock_sender: sender,
+            flashblock_sender: sender,
+            flashblocks_state,
+            provider,
             _node_exit_future: node_exit_future,
             _node: Box::new(node_handle),
             _task_manager: tasks,
         })
     }
 
-    // pub async fn send_flashblock(&self, flashblock: Flashblock) -> Result<()> {
-    //     let (tx, rx) = oneshot::channel();
-    //     self.flashblock_sender.send((flashblock, tx)).await?;
-    //     rx.await?;
-    //     Ok(())
-    // }
+    pub async fn send_flashblock(&self, flashblock: Flashblock) -> Result<()> {
+        let (tx, rx) = oneshot::channel();
+        self.flashblock_sender
+            .send((flashblock, tx))
+            .await
+            .map_err(|err| eyre::eyre!(err))?;
+        rx.await.map_err(|err| eyre::eyre!(err))?;
+        Ok(())
+    }
 
     pub fn provider(&self) -> Result<RootProvider<Optimism>> {
         let url = format!("http://{}", self.http_api_addr);
@@ -180,5 +218,13 @@ impl LocalNode {
 
     pub fn engine_api(&self) -> Result<EngineApi<crate::engine::IpcEngine>> {
         EngineApi::<crate::engine::IpcEngine>::new(self.engine_ipc_path.clone())
+    }
+
+    pub fn flashblocks_state(&self) -> Arc<LocalFlashblocksState> {
+        self.flashblocks_state.clone()
+    }
+
+    pub fn blockchain_provider(&self) -> LocalNodeProvider {
+        self.provider.clone()
     }
 }
