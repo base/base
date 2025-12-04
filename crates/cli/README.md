cab1852ef9757ea29b4f8e00c5640c3571667d48
refcell <abigger87@gmail.com>
2025-12-04 13:15:36 -0500

    feat(cli): refactor cli utils out to reduce binary clutter

---
+++ crates/cli/README.md
@@ -0,0 +1,3 @@
+# CLI Utilities for Base Reth
+
+Contains a set of utilities for the Base Reth node CLI.
---
+++ crates/cli/src/lib.rs
@@ -0,0 +1,7 @@
+#![doc = include_str!("../README.md")]
+#![doc(issue_tracker_base_url = "https://github.com/base/node-reth/issues/")]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
+mod version;
+pub use version::Version;
