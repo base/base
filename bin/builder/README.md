22b968b29cbb3f6cef5292b73a2ed39b5aee7a0c
Andreas Bigger <andreas.bigger@coinbase.com>
2026-01-14 12:24:33 -0500

    chore(bin): fix builder binary

---
+++ bin/builder/README.md
@@ -0,0 +1,5 @@
+# `base-builder`
+
+The block builder for the Base Stack.
+
+Under the hood, `base-builder` runs [op-rbuilder](../../crates/op-rbuilder/).
--- bin/builder/src/main.rs
+++ bin/builder/src/main.rs
@@ -1,14 +1,17 @@
-#![doc = "OP-Rbuilder Binary"]
+#![doc = include_str!("../README.md")]
 #![doc(issue_tracker_base_url = "https://github.com/base/node-reth/issues/")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-use eyre::Result;
-use op_rbuilder::launcher::launch;
-
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
 
-fn main() -> Result<()> {
-    launch()
+fn main() {
+    base_cli_utils::Backtracing::enable();
+    base_cli_utils::SigsegvHandler::install();
+
+    if let Err(err) = op_rbuilder::launcher::launch() {
+        eprintln!("Error: {err:?}");
+        std::process::exit(1);
+    }
 }
