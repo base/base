c10feca92b60c6f1de376aaf65bd120dd34ba068
Julian Meyer <julian.meyer@coinbase.com>
2026-01-14 15:37:34 -0800

    feat: remove extra OP-specific addons (#470)
    * chore: separate add-ons from base node
    
    * feat: remove extra OP-specific addons
    
    Removes: historical RPC, flashblocks, DebugNode
    
    * Remove tx conditional support
    
    * fmt

---
+++ crates/client/node/src/add_ons.rs
@@ -0,0 +1,443 @@
+use std::marker::PhantomData;
+
+use base_client_engine::BaseEngineValidatorBuilder;
+use reth_evm::ConfigureEvm;
+use reth_node_api::{BuildNextEnv, FullNodeComponents, HeaderTy, NodeAddOns, PayloadTypes, TxTy};
+use reth_node_builder::{
+    node::NodeTypes,
+    rpc::{
+        EngineApiBuilder, EngineValidatorAddOn, EngineValidatorBuilder, EthApiBuilder, Identity,
+        PayloadValidatorBuilder, RethRpcAddOns, RethRpcMiddleware, RethRpcServerHandles, RpcAddOns,
+        RpcContext, RpcHandle,
+    },
+};
+use reth_optimism_forks::OpHardforks;
+use reth_optimism_node::{OpEngineApiBuilder, OpEngineValidatorBuilder, OpNodeTypes};
+use reth_optimism_payload_builder::{
+    OpAttributes, OpPayloadPrimitives,
+    config::{OpDAConfig, OpGasLimitConfig},
+};
+use reth_optimism_rpc::{
+    eth::OpEthApiBuilder,
+    miner::{MinerApiExtServer, OpMinerExtApi},
+    witness::OpDebugWitnessApi,
+};
+use reth_optimism_txpool::OpPooledTx;
+use reth_rpc_api::{DebugApiServer, DebugExecutionWitnessApiServer};
+use reth_rpc_server_types::RethRpcModule;
+use reth_tracing::tracing::debug;
+use reth_transaction_pool::TransactionPool;
+use serde::de::DeserializeOwned;
+
+/// Add-ons w.r.t. optimism.
+///
+/// This type provides optimism-specific addons to the node and exposes the RPC server and engine
+/// API.
+#[derive(Debug)]
+pub struct BaseAddOns<
+    N: FullNodeComponents,
+    EthB: EthApiBuilder<N>,
+    PVB,
+    EB = OpEngineApiBuilder<PVB>,
+    EVB = BaseEngineValidatorBuilder<PVB>,
+    RpcMiddleware = Identity,
+> {
+    /// Rpc add-ons responsible for launching the RPC servers and instantiating the RPC handlers
+    /// and eth-api.
+    pub rpc_add_ons: RpcAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>,
+    /// Data availability configuration for the OP builder.
+    pub da_config: OpDAConfig,
+    /// Gas limit configuration for the OP builder.
+    pub gas_limit_config: OpGasLimitConfig,
+    min_suggested_priority_fee: u64,
+}
+
+impl<N, EthB, PVB, EB, EVB, RpcMiddleware> BaseAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>
+where
+    N: FullNodeComponents,
+    EthB: EthApiBuilder<N>,
+{
+    /// Creates a new instance from components.
+    #[allow(clippy::too_many_arguments)]
+    pub const fn new(
+        rpc_add_ons: RpcAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>,
+        da_config: OpDAConfig,
+        gas_limit_config: OpGasLimitConfig,
+        min_suggested_priority_fee: u64,
+    ) -> Self {
+        Self { rpc_add_ons, da_config, gas_limit_config, min_suggested_priority_fee }
+    }
+}
+
+impl<N> Default for BaseAddOns<N, OpEthApiBuilder, OpEngineValidatorBuilder>
+where
+    N: FullNodeComponents<Types: OpNodeTypes>,
+    OpEthApiBuilder: EthApiBuilder<N>,
+{
+    fn default() -> Self {
+        Self::builder().build()
+    }
+}
+
+impl<N, NetworkT, RpcMiddleware>
+    BaseAddOns<
+        N,
+        OpEthApiBuilder<NetworkT>,
+        OpEngineValidatorBuilder,
+        OpEngineApiBuilder<OpEngineValidatorBuilder>,
+        RpcMiddleware,
+    >
+where
+    N: FullNodeComponents<Types: OpNodeTypes>,
+    OpEthApiBuilder<NetworkT>: EthApiBuilder<N>,
+{
+    /// Build a [`OpAddOns`] using [`OpAddOnsBuilder`].
+    pub fn builder() -> BaseAddOnsBuilder<NetworkT> {
+        BaseAddOnsBuilder::default()
+    }
+}
+
+impl<N, EthB, PVB, EB, EVB, RpcMiddleware> BaseAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>
+where
+    N: FullNodeComponents,
+    EthB: EthApiBuilder<N>,
+{
+    /// Maps the [`reth_node_builder::rpc::EngineApiBuilder`] builder type.
+    pub fn with_engine_api<T>(
+        self,
+        engine_api_builder: T,
+    ) -> BaseAddOns<N, EthB, PVB, T, EVB, RpcMiddleware> {
+        let Self { rpc_add_ons, da_config, gas_limit_config, min_suggested_priority_fee, .. } =
+            self;
+        BaseAddOns::new(
+            rpc_add_ons.with_engine_api(engine_api_builder),
+            da_config,
+            gas_limit_config,
+            min_suggested_priority_fee,
+        )
+    }
+
+    /// Maps the [`PayloadValidatorBuilder`] builder type.
+    pub fn with_payload_validator<T>(
+        self,
+        payload_validator_builder: T,
+    ) -> BaseAddOns<N, EthB, T, EB, EVB, RpcMiddleware> {
+        let Self { rpc_add_ons, da_config, gas_limit_config, min_suggested_priority_fee, .. } =
+            self;
+        BaseAddOns::new(
+            rpc_add_ons.with_payload_validator(payload_validator_builder),
+            da_config,
+            gas_limit_config,
+            min_suggested_priority_fee,
+        )
+    }
+
+    /// Sets the RPC middleware stack for processing RPC requests.
+    ///
+    /// This method configures a custom middleware stack that will be applied to all RPC requests
+    /// across HTTP, `WebSocket`, and IPC transports. The middleware is applied to the RPC service
+    /// layer, allowing you to intercept, modify, or enhance RPC request processing.
+    ///
+    /// See also [`RpcAddOns::with_rpc_middleware`].
+    pub fn with_rpc_middleware<T>(self, rpc_middleware: T) -> BaseAddOns<N, EthB, PVB, EB, EVB, T> {
+        let Self { rpc_add_ons, da_config, gas_limit_config, min_suggested_priority_fee, .. } =
+            self;
+        BaseAddOns::new(
+            rpc_add_ons.with_rpc_middleware(rpc_middleware),
+            da_config,
+            gas_limit_config,
+            min_suggested_priority_fee,
+        )
+    }
+
+    /// Sets the hook that is run once the rpc server is started.
+    pub fn on_rpc_started<F>(mut self, hook: F) -> Self
+    where
+        F: FnOnce(RpcContext<'_, N, EthB::EthApi>, RethRpcServerHandles) -> eyre::Result<()>
+            + Send
+            + 'static,
+    {
+        self.rpc_add_ons = self.rpc_add_ons.on_rpc_started(hook);
+        self
+    }
+
+    /// Sets the hook that is run to configure the rpc modules.
+    pub fn extend_rpc_modules<F>(mut self, hook: F) -> Self
+    where
+        F: FnOnce(RpcContext<'_, N, EthB::EthApi>) -> eyre::Result<()> + Send + 'static,
+    {
+        self.rpc_add_ons = self.rpc_add_ons.extend_rpc_modules(hook);
+        self
+    }
+}
+
+impl<N, EthB, PVB, EB, EVB, Attrs, RpcMiddleware> NodeAddOns<N>
+    for BaseAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>
+where
+    N: FullNodeComponents<
+            Types: NodeTypes<
+                ChainSpec: OpHardforks,
+                Primitives: OpPayloadPrimitives,
+                Payload: PayloadTypes<PayloadBuilderAttributes = Attrs>,
+            >,
+            Evm: ConfigureEvm<
+                NextBlockEnvCtx: BuildNextEnv<
+                    Attrs,
+                    HeaderTy<N::Types>,
+                    <N::Types as NodeTypes>::ChainSpec,
+                >,
+            >,
+            Pool: TransactionPool<Transaction: OpPooledTx>,
+        >,
+    EthB: EthApiBuilder<N>,
+    PVB: Send,
+    EB: EngineApiBuilder<N>,
+    EVB: EngineValidatorBuilder<N>,
+    RpcMiddleware: RethRpcMiddleware,
+    Attrs: OpAttributes<Transaction = TxTy<N::Types>, RpcPayloadAttributes: DeserializeOwned>,
+{
+    type Handle = RpcHandle<N, EthB::EthApi>;
+
+    async fn launch_add_ons(
+        self,
+        ctx: reth_node_api::AddOnsContext<'_, N>,
+    ) -> eyre::Result<Self::Handle> {
+        let Self { rpc_add_ons, da_config, gas_limit_config, .. } = self;
+
+        let builder = reth_optimism_payload_builder::OpPayloadBuilder::new(
+            ctx.node.pool().clone(),
+            ctx.node.provider().clone(),
+            ctx.node.evm_config().clone(),
+        );
+        // install additional OP specific rpc methods
+        let debug_ext = OpDebugWitnessApi::<_, _, _, Attrs>::new(
+            ctx.node.provider().clone(),
+            Box::new(ctx.node.task_executor().clone()),
+            builder,
+        );
+        let miner_ext = OpMinerExtApi::new(da_config, gas_limit_config);
+
+        rpc_add_ons
+            .launch_add_ons_with(ctx, move |container| {
+                let reth_node_builder::rpc::RpcModuleContainer { modules, auth_module, registry } =
+                    container;
+
+                debug!(target: "reth::cli", "Installing debug payload witness rpc endpoint");
+                modules.merge_if_module_configured(RethRpcModule::Debug, debug_ext.into_rpc())?;
+
+                // extend the miner namespace if configured in the regular http server
+                modules.add_or_replace_if_module_configured(
+                    RethRpcModule::Miner,
+                    miner_ext.clone().into_rpc(),
+                )?;
+
+                // install the miner extension in the authenticated if configured
+                if modules.module_config().contains_any(&RethRpcModule::Miner) {
+                    debug!(target: "reth::cli", "Installing miner DA rpc endpoint");
+                    auth_module.merge_auth_methods(miner_ext.into_rpc())?;
+                }
+
+                // install the debug namespace in the authenticated if configured
+                if modules.module_config().contains_any(&RethRpcModule::Debug) {
+                    debug!(target: "reth::cli", "Installing debug rpc endpoint");
+                    auth_module.merge_auth_methods(registry.debug_api().into_rpc())?;
+                }
+
+                Ok(())
+            })
+            .await
+    }
+}
+
+impl<N, EthB, PVB, EB, EVB, Attrs, RpcMiddleware> RethRpcAddOns<N>
+    for BaseAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>
+where
+    N: FullNodeComponents<
+            Types: NodeTypes<
+                ChainSpec: OpHardforks,
+                Primitives: OpPayloadPrimitives,
+                Payload: PayloadTypes<PayloadBuilderAttributes = Attrs>,
+            >,
+            Evm: ConfigureEvm<
+                NextBlockEnvCtx: BuildNextEnv<
+                    Attrs,
+                    HeaderTy<N::Types>,
+                    <N::Types as NodeTypes>::ChainSpec,
+                >,
+            >,
+        >,
+    <<N as FullNodeComponents>::Pool as TransactionPool>::Transaction: OpPooledTx,
+    EthB: EthApiBuilder<N>,
+    PVB: PayloadValidatorBuilder<N>,
+    EB: EngineApiBuilder<N>,
+    EVB: EngineValidatorBuilder<N>,
+    RpcMiddleware: RethRpcMiddleware,
+    Attrs: OpAttributes<Transaction = TxTy<N::Types>, RpcPayloadAttributes: DeserializeOwned>,
+{
+    type EthApi = EthB::EthApi;
+
+    fn hooks_mut(&mut self) -> &mut reth_node_builder::rpc::RpcHooks<N, Self::EthApi> {
+        self.rpc_add_ons.hooks_mut()
+    }
+}
+
+impl<N, EthB, PVB, EB, EVB, RpcMiddleware> EngineValidatorAddOn<N>
+    for BaseAddOns<N, EthB, PVB, EB, EVB, RpcMiddleware>
+where
+    N: FullNodeComponents,
+    EthB: EthApiBuilder<N>,
+    PVB: Send,
+    EB: EngineApiBuilder<N>,
+    EVB: EngineValidatorBuilder<N>,
+    RpcMiddleware: Send,
+{
+    type ValidatorBuilder = EVB;
+
+    fn engine_validator_builder(&self) -> Self::ValidatorBuilder {
+        EngineValidatorAddOn::engine_validator_builder(&self.rpc_add_ons)
+    }
+}
+
+/// A regular optimism evm and executor builder.
+#[derive(Debug, Clone)]
+#[non_exhaustive]
+pub struct BaseAddOnsBuilder<NetworkT, RpcMiddleware = Identity> {
+    /// Sequencer client, configured to forward submitted transactions to sequencer of given OP
+    /// network.
+    sequencer_url: Option<String>,
+    /// Headers to use for the sequencer client requests.
+    sequencer_headers: Vec<String>,
+    /// Data availability configuration for the OP builder.
+    da_config: Option<OpDAConfig>,
+    /// Gas limit configuration for the OP builder.
+    gas_limit_config: Option<OpGasLimitConfig>,
+    /// Marker for network types.
+    _nt: PhantomData<NetworkT>,
+    /// Minimum suggested priority fee (tip)
+    min_suggested_priority_fee: u64,
+    /// RPC middleware to use
+    rpc_middleware: RpcMiddleware,
+    /// Optional tokio runtime to use for the RPC server.
+    tokio_runtime: Option<tokio::runtime::Handle>,
+}
+
+impl<NetworkT> Default for BaseAddOnsBuilder<NetworkT> {
+    fn default() -> Self {
+        Self {
+            sequencer_url: None,
+            sequencer_headers: Vec::new(),
+            da_config: None,
+            gas_limit_config: None,
+            min_suggested_priority_fee: 1_000_000,
+            _nt: PhantomData,
+            rpc_middleware: Identity::new(),
+            tokio_runtime: None,
+        }
+    }
+}
+
+impl<NetworkT, RpcMiddleware> BaseAddOnsBuilder<NetworkT, RpcMiddleware> {
+    /// With a [`SequencerClient`].
+    pub fn with_sequencer(mut self, sequencer_client: Option<String>) -> Self {
+        self.sequencer_url = sequencer_client;
+        self
+    }
+
+    /// With headers to use for the sequencer client requests.
+    pub fn with_sequencer_headers(mut self, sequencer_headers: Vec<String>) -> Self {
+        self.sequencer_headers = sequencer_headers;
+        self
+    }
+
+    /// Configure the data availability configuration for the OP builder.
+    pub fn with_da_config(mut self, da_config: OpDAConfig) -> Self {
+        self.da_config = Some(da_config);
+        self
+    }
+
+    /// Configure the gas limit configuration for the OP payload builder.
+    pub fn with_gas_limit_config(mut self, gas_limit_config: OpGasLimitConfig) -> Self {
+        self.gas_limit_config = Some(gas_limit_config);
+        self
+    }
+
+    /// Configure the minimum priority fee (tip)
+    pub const fn with_min_suggested_priority_fee(mut self, min: u64) -> Self {
+        self.min_suggested_priority_fee = min;
+        self
+    }
+
+    /// Configures a custom tokio runtime for the RPC server.
+    ///
+    /// Caution: This runtime must not be created from within asynchronous context.
+    pub fn with_tokio_runtime(mut self, tokio_runtime: Option<tokio::runtime::Handle>) -> Self {
+        self.tokio_runtime = tokio_runtime;
+        self
+    }
+
+    /// Configure the RPC middleware to use
+    pub fn with_rpc_middleware<T>(self, rpc_middleware: T) -> BaseAddOnsBuilder<NetworkT, T> {
+        let Self {
+            sequencer_url,
+            sequencer_headers,
+            da_config,
+            gas_limit_config,
+            min_suggested_priority_fee,
+            tokio_runtime,
+            _nt,
+            ..
+        } = self;
+        BaseAddOnsBuilder {
+            sequencer_url,
+            sequencer_headers,
+            da_config,
+            gas_limit_config,
+            min_suggested_priority_fee,
+            _nt,
+            rpc_middleware,
+            tokio_runtime,
+        }
+    }
+}
+
+impl<NetworkT, RpcMiddleware> BaseAddOnsBuilder<NetworkT, RpcMiddleware> {
+    /// Builds an instance of [`OpAddOns`].
+    pub fn build<N, PVB, EB, EVB>(
+        self,
+    ) -> BaseAddOns<N, OpEthApiBuilder<NetworkT>, PVB, EB, EVB, RpcMiddleware>
+    where
+        N: FullNodeComponents<Types: NodeTypes>,
+        OpEthApiBuilder<NetworkT>: EthApiBuilder<N>,
+        PVB: PayloadValidatorBuilder<N> + Default,
+        EB: Default,
+        EVB: Default,
+    {
+        let Self {
+            sequencer_url,
+            sequencer_headers,
+            da_config,
+            gas_limit_config,
+            min_suggested_priority_fee,
+            rpc_middleware,
+            tokio_runtime,
+            ..
+        } = self;
+
+        BaseAddOns::new(
+            RpcAddOns::new(
+                OpEthApiBuilder::default()
+                    .with_sequencer(sequencer_url)
+                    .with_sequencer_headers(sequencer_headers)
+                    .with_min_suggested_priority_fee(min_suggested_priority_fee),
+                PVB::default(),
+                EB::default(),
+                EVB::default(),
+                rpc_middleware,
+            )
+            .with_tokio_runtime(tokio_runtime),
+            da_config.unwrap_or_default(),
+            gas_limit_config.unwrap_or_default(),
+            min_suggested_priority_fee,
+        )
+    }
+}
