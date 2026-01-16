99d377c959d3c5a2423fc29ed7e2a31a36cd8f47
William Law <williamlaw.wtl@gmail.com>
2026-01-16 16:31:27 -0500

    chore: fix primitives feature flag issue (#510)

--- crates/shared/primitives/src/lib.rs
+++ crates/shared/primitives/src/lib.rs
@@ -3,9 +3,9 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-#[cfg(any(test, feature = "test-utils"))]
+#[cfg(feature = "test-utils")]
 pub mod test_utils;
 
 // These dependencies are only used in test-utils feature
-#[cfg(any(test, feature = "test-utils"))]
+#[cfg(feature = "test-utils")]
 pub use test_utils::*;
