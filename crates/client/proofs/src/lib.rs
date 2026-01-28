cae87f230a1e44e4ee97686e745450bd7496038e
Julian Meyer <julian.meyer@coinbase.com>
2026-01-28 08:18:22 -0800

    feat: integrate proofs ExEx to Base (#579)

---
+++ crates/client/proofs/src/lib.rs
@@ -0,0 +1,7 @@
+#![doc = include_str!("../README.md")]
+#![doc(issue_tracker_base_url = "https://github.com/base/base/issues/")]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
+mod proofs;
+pub use proofs::{ProofsHistoryConfig, ProofsHistoryExtension};
