cd3dcf73f0df9a0617fbee6c86484cf6448c3b6f
Haardik H <haardik.haardik@coinbase.com>
2026-02-02 11:56:15 -0500

    limit unique names to alphanumeric ids only

--- system_tests/src/lib.rs
+++ system_tests/src/lib.rs
@@ -47,9 +47,14 @@ pub mod cli;
 pub mod config;
 pub mod devnet_config;
 
+const ALPHANUMERIC: &[char] = &[
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+];
+
 /// Generates a unique container name with the given prefix.
 pub fn unique_name(prefix: &str) -> String {
-    format!("{}-{}", prefix, nanoid::nanoid!(8))
+    format!("{}-{}", prefix, nanoid::nanoid!(8, ALPHANUMERIC))
 }
 /// Container orchestration and management.
 pub mod containers;
