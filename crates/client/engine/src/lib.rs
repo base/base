374075e8c06e3cbfcae1bbb3f3ff96c7362c5057
Julian Meyer <julian.meyer@coinbase.com>
2026-02-21 01:19:06 +0000

    make flashblock cached execution provider pub

--- crates/client/engine/src/lib.rs
+++ crates/client/engine/src/lib.rs
@@ -3,4 +3,4 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 mod validator;
-pub use validator::BaseEngineValidatorBuilder;
+pub use validator::{BaseEngineValidatorBuilder, FlashblocksCachedExecutionProvider};
