793cb801b90ef6208e1ce1b84ca2205c04dc10d9
Jack Chuma <jack.chuma@coinbase.com>
2026-03-09 18:45:57 -0400

    empty impl

--- crates/utilities/tx-manager/src/manager.rs
+++ crates/utilities/tx-manager/src/manager.rs
@@ -1,5 +1,9 @@
 //! Core transaction manager implementation.
 
+use crate::TxManager;
+
 /// Default transaction manager implementation.
 #[derive(Debug)]
 pub struct SimpleTxManager;
+
+impl TxManager for SimpleTxManager {}
