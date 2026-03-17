b28f5b9f564615009324ad2cd30fdc128584da1f
Jack Chuma <jack.chuma@coinbase.com>
2026-03-16 22:00:05 -0400

    integrate tx manager to proposer

--- crates/proof/proposer/src/lib.rs
+++ crates/proof/proposer/src/lib.rs
@@ -13,15 +13,14 @@ mod cli;
 pub use cli::{Cli, LogArgs, MetricsArgs, ProposerArgs, RpcServerArgs};
 
 mod config;
-pub use config::{ConfigError, ProposerConfig, RpcServerConfig, SigningConfig};
+pub use config::{ConfigError, ProposerConfig, RpcServerConfig};
 
 mod constants;
 pub use constants::*;
 
 mod output_proposer;
 pub use output_proposer::{
-    LocalOutputProposer, OutputProposer, RemoteOutputProposer, build_proof_data,
-    create_output_proposer, is_game_already_exists,
+    OutputProposer, ProposalSubmitter, build_proof_data, is_game_already_exists,
 };
 
 mod driver;
