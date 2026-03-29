0811fe22afb5eb8b9072042e5901500d95e396bd
AugusJonsonqbv <xlffiojsu17@gmx.com>
2026-03-29 16:28:11 +0800

    chore(builder-core): warn unused deps; slim Cargo.toml for test-utils

--- crates/builder/core/src/lib.rs
+++ crates/builder/core/src/lib.rs
@@ -6,7 +6,7 @@
     issue_tracker_base_url = "https://github.com/base/base/issues/"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![cfg_attr(not(test), allow(unused_crate_dependencies))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 mod config;
 pub use config::BuilderConfig;
