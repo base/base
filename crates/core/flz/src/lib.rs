b7d4170c1b2317f5d509c3f66ba700d577093010
Andreas Bigger <andreas.bigger@coinbase.com>
2026-04-02 15:12:02 -0400

    fix fmts

--- crates/core/flz/src/lib.rs
+++ crates/core/flz/src/lib.rs
@@ -3,7 +3,6 @@
 
 mod flz;
 pub use flz::{
-    L1_COST_FASTLZ_COEF, L1_COST_INTERCEPT, MIN_TX_SIZE_SCALED, NON_ZERO_BYTE_COST,
-    ZERO_BYTE_COST, data_gas_fjord, flz_compress_len, tx_estimated_size_fjord,
-    tx_estimated_size_fjord_bytes,
+    L1_COST_FASTLZ_COEF, L1_COST_INTERCEPT, MIN_TX_SIZE_SCALED, NON_ZERO_BYTE_COST, ZERO_BYTE_COST,
+    data_gas_fjord, flz_compress_len, tx_estimated_size_fjord, tx_estimated_size_fjord_bytes,
 };
