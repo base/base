15edf9cd90c264ab88ed334eb3a222cd9738e327
Andreas Bigger <andreas.bigger@coinbase.com>
2026-04-30 14:28:21 -0400

    chore(deps): bump reth to v1.11.4

--- devnet/src/images.rs
+++ devnet/src/images.rs
@@ -1,7 +1,7 @@
 //! Docker image constants for devnet containers.
 
 /// Docker image for Reth.
-pub const RETH_IMAGE: &str = "ghcr.io/paradigmxyz/reth:v1.11.3";
+pub const RETH_IMAGE: &str = "ghcr.io/paradigmxyz/reth:v1.11.4";
 /// Docker image for op-deployer.
 pub const OP_DEPLOYER_IMAGE: &str =
     "us-docker.pkg.dev/oplabs-tools-artifacts/images/op-deployer:v0.6.0-rc.3";
