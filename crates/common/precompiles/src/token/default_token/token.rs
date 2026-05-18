e93defed5de2ccc7cf4297ebb011894fbdc58035
Rayyan Alam <rayyan.alam@coinbase.com>
2026-05-18 11:19:17 -0400

    chore: formating

--- crates/common/precompiles/src/token/default_token/token.rs
+++ crates/common/precompiles/src/token/default_token/token.rs
@@ -69,4 +69,3 @@ impl<S: TokenAccounting> Redeemable for DefaultToken<S> {}
 impl<S: TokenAccounting> Pausable for DefaultToken<S> {}
 impl<S: TokenAccounting> Configurable for DefaultToken<S> {}
 impl<S: TokenAccounting> Permittable for DefaultToken<S> {}
-
