8ea38d50b7805e28d95996bfe280548dc6bc978f
Niran Babalola <niran.babalola@coinbase.com>
2026-06-23 10:29:15 -0500

    Use global transaction event writer type

--- bin/builder/src/main.rs
+++ bin/builder/src/main.rs
@@ -10,7 +10,7 @@ use base_builder_core::{BuilderApiExtension, FlashblocksServiceBuilder};
 use base_builder_metering::MeteringStoreExtension;
 use base_execution_cli::{Cli, StandardBaseRethNode};
 use base_node_runner::BaseNodeRunner;
-use base_observability_events::init_global_transaction_event_writer;
+use base_observability_events::GlobalTransactionEventWriter;
 use base_txpool_rpc::{TxPoolRpcConfig, TxPoolRpcExtension};
 
 type BuilderCli = Cli<Args>;
@@ -36,7 +36,7 @@ fn main() {
         let metering_provider: base_builder_core::SharedMeteringProvider =
             Arc::new(builder_args.build_metering_store());
         let transaction_events_enabled = builder_args.transaction_events.enabled;
-        init_global_transaction_event_writer(
+        GlobalTransactionEventWriter::init(
             transaction_events_enabled.then(|| builder_args.transaction_events.writer_config()),
         )
         .await?;
