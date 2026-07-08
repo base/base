9d196e1b60dafd0c5a1737b543693686f71ceb49
Francis Li <francis.li@coinbase.com>
2026-07-08 21:55:34 +0000

    test to ensure parity without ms timestamp

--- crates/common/rpc-types-engine/src/attributes.rs
+++ crates/common/rpc-types-engine/src/attributes.rs
@@ -353,6 +353,33 @@ mod test {
     }
 
     #[test]
+    fn test_payload_id_unchanged_without_timestamp_millis_part() {
+        const PAYLOAD_VERSION: u8 = 3;
+
+        let expected =
+            PayloadId::new(FixedBytes::<8>::from_str("0x03d2dae446d2a86a").unwrap().into());
+        let parent = b256!("0x3533bf30edaf9505d0810bf475cbe4e5f4b9889904b9845e83efdeab4e92eb1e");
+        let attrs = BasePayloadAttributes {
+            payload_attributes: PayloadAttributes {
+                timestamp: 1728933301,
+                prev_randao: b256!("0x9158595abbdab2c90635087619aa7042bbebe47642dfab3c9bfb934f6b082765"),
+                suggested_fee_recipient: address!("0x4200000000000000000000000000000000000011"),
+                withdrawals: Some([].into()),
+                parent_beacon_block_root: b256!("0x8fe0193b9bf83cb7e5a08538e494fecc23046aab9a497af3704f4afdae3250ff").into(),
+                slot_number: None,
+            },
+            transactions: Some([bytes!("7ef8f8a0dc19cfa777d90980e4875d0a548a881baaa3f83f14d1bc0d3038bc329350e54194deaddeaddeaddeaddeaddeaddeaddeaddead00019442000000000000000000000000000000000000158080830f424080b8a4440a5e20000f424000000000000000000000000300000000670d6d890000000000000125000000000000000000000000000000000000000000000000000000000000000700000000000000000000000000000000000000000000000000000000000000014bf9181db6e381d4384bbf69c48b0ee0eed23c6ca26143c6d2544f9d39997a590000000000000000000000007f83d659683caf2767fd3c720981d51f5bc365bc")].into()),
+            no_tx_pool: None,
+            gas_limit: Some(30000000),
+            eip_1559_params: None,
+            min_base_fee: None,
+            timestamp_millis_part: None,
+        };
+
+        assert_eq!(attrs.payload_id(&parent, PAYLOAD_VERSION), expected);
+    }
+
+    #[test]
     fn test_serde_roundtrip_attributes_post_holocene() {
         let attributes = BasePayloadAttributes {
             payload_attributes: PayloadAttributes {
