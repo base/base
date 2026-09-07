7fef11f0148436f9a9fa91d710259bdd003162e4
Danyal Prout <git@dany.al>
2026-09-07 21:54:20 +0000

    Remove unused TxEnvConverter strategy trait

--- vendor/reth-rpc-convert/src/transaction.rs
+++ vendor/reth-rpc-convert/src/transaction.rs
@@ -1,14 +1,10 @@
 //! Compatibility functions for rpc `Transaction` type.
-use core::error;
 use std::fmt::Debug;
 
 use alloy_consensus::transaction::Recovered;
 use alloy_rpc_types_eth::Log;
 use base_common_consensus::{BaseBlock, BaseReceipt, BaseTxEnvelope};
-use reth_evm::{BlockEnvFor, EvmEnvFor, SpecFor, TxEnvFor};
 use reth_primitives_traits::{SealedBlock, TransactionMeta};
-
-use crate::TryIntoTxEnv;
 
 /// Primitive receipt and transaction context used to construct a Base RPC receipt.
 #[derive(Debug, Clone)]
@@ -58,56 +54,6 @@ pub trait ReceiptConverter: Debug + 'static {
         _block: &SealedBlock<BaseBlock>,
     ) -> Result<Vec<Self::RpcReceipt>, Self::Error> {
         self.convert_receipts(receipts)
-    }
-}
-
-/// Converts `TxReq` into `TxEnv`.
-///
-/// Where:
-/// * `TxReq` is a transaction request received from an RPC API
-/// * `TxEnv` is the corresponding transaction environment for execution
-///
-/// The `TxEnvConverter` has two blanket implementations:
-/// * `()` assuming `TxReq` implements [`TryIntoTxEnv`] and is used as default for `BaseRpcConverter`.
-/// * `Fn(TxReq, &CfgEnv<Spec>, &BlockEnv) -> Result<TxEnv, E>` and can be applied using
-///   a custom transaction environment conversion function.
-///
-/// One should prefer to implement [`TryIntoTxEnv`] for `TxReq` to get the `TxEnvConverter`
-/// implementation for free, thanks to the blanket implementation, unless the conversion requires
-/// more context. For example, some configuration parameters or access handles to database, network,
-/// etc.
-pub trait TxEnvConverter<TxReq>: Debug + Send + Sync + Unpin + Clone + 'static {
-    /// An associated error that can occur during conversion.
-    type Error;
-
-    /// Converts a rpc transaction request into a transaction environment.
-    ///
-    /// See [`TxEnvConverter`] for more information.
-    fn convert_tx_env(&self, tx_req: TxReq, evm_env: &EvmEnvFor) -> Result<TxEnvFor, Self::Error>;
-}
-
-impl<TxReq> TxEnvConverter<TxReq> for ()
-where
-    TxReq: TryIntoTxEnv<TxEnvFor, SpecFor, BlockEnvFor>,
-{
-    type Error = TxReq::Err;
-
-    fn convert_tx_env(&self, tx_req: TxReq, evm_env: &EvmEnvFor) -> Result<TxEnvFor, Self::Error> {
-        tx_req.try_into_tx_env(evm_env)
-    }
-}
-
-/// Converts rpc transaction requests into transaction environment using a closure.
-impl<F, TxReq, E> TxEnvConverter<TxReq> for F
-where
-    F: Fn(TxReq, &EvmEnvFor) -> Result<TxEnvFor, E> + Debug + Send + Sync + Unpin + Clone + 'static,
-    TxReq: Clone,
-    E: error::Error + Send + Sync + 'static,
-{
-    type Error = E;
-
-    fn convert_tx_env(&self, tx_req: TxReq, evm_env: &EvmEnvFor) -> Result<TxEnvFor, Self::Error> {
-        self(tx_req, evm_env)
     }
 }
 
