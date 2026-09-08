9d81fcfb4bf47329d3667bf516d7d2057e4ac308
Danyal Prout <git@dany.al>
2026-09-08 20:13:47 +0000

    refactor(deps): use upstream nybbles

--- vendor/nybbles/README.md
+++
@@ -1,3 +0,0 @@
-# nybbles
-
-Efficient nibble-sized (4-bit) byte sequence data structure.
--- vendor/nybbles/benches/benches/clone.rs
+++
@@ -1,7 +0,0 @@
-use crate::prelude::*;
-
-pub fn group(criterion: &mut Criterion) {
-    for &size in &SIZE_NIBBLES {
-        bench_unop(criterion, "clone", size, black_box);
-    }
-}
