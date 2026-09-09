4bf142e81e763cd8736fd8db0fdd43c6e228848e
Danyal Prout <git@dany.al>
2026-09-09 13:27:19 +0000

    Use upstream Alloy Solidity macro crates

--- vendor/alloy-sol-macro-input/src/expander.rs
+++
@@ -1,9 +0,0 @@
-use proc_macro2::TokenStream;
-
-use crate::SolInput;
-
-/// Expands a `SolInput` into a `TokenStream`.
-pub trait SolInputExpander {
-    /// Expand a `SolInput` into a `TokenStream`.
-    fn expand(&mut self, input: &SolInput) -> syn::Result<TokenStream>;
-}
